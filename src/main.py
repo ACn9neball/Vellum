f020a01342bf271542eea281811c9dc6b33205af
AC Nine <bhekainnocentn@gmail.com>
2026-07-10 13:41:06 +0200

    Save settings

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 import sys
+import json
 from pathlib import Path
 from PyQt6.QtGui import QIcon
 from PyQt6.QtWidgets import (
     QApplication,
+    QFileDialog,
     QFrame,
     QHBoxLayout,
     QLabel,
@@ -13,9 +15,12 @@ from PyQt6.QtWidgets import (
     QVBoxLayout,
     QWidget,
 )
+from platformdirs import user_config_dir
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 ASSETS_DIR = BASE_DIR / "assets"
+APP_NAME = "Vellum"
+APP_AUTHOR = "Personal"
 
 
 class MainWindow(QMainWindow):
@@ -23,6 +28,11 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("Vellum")
         container = QWidget()
+
+        self.config_dir = Path(user_config_dir(APP_NAME, APP_AUTHOR))
+        self.settings_file = self.config_dir / "settings.json"
+        self.default_settings = self.load_settings()
+
         layout = QHBoxLayout(container)
         layout.setContentsMargins(0, 0, 0, 0)
         layout.setSpacing(0)
@@ -68,17 +78,44 @@ class MainWindow(QMainWindow):
         )
         path_layout = QHBoxLayout(path_container)
 
-        lblPath = QLabel("Default Manga Path")
-        ledPath = QLineEdit()
+        lblPath = QLabel("Default Manga Path:")
+        self.ledPath = QLineEdit()
+        self.ledPath.setText(self.default_settings["folder_path"])
         iconFolder = QIcon(str(ASSETS_DIR / "folder-open.png"))
         btnPath = QPushButton()
         btnPath.setIcon(iconFolder)
+        btnPath.clicked.connect(self.pathDefault)
 
         path_layout.addWidget(lblPath, stretch=2)
-        path_layout.addWidget(ledPath, stretch=5)
+        path_layout.addWidget(self.ledPath, stretch=7)
         path_layout.addWidget(btnPath, stretch=1)
 
         self.right_layout.addWidget(path_container)
+
+    def pathDefault(self):
+        folderPath = QFileDialog.getExistingDirectory(
+            None, "Select File", self.default_settings["folder_path"]
+        )
+        default_settings = {"theme": "light", "folder_path": folderPath}
+        self.save_settings(default_settings)
+        self.ledPath.setText(self.default_settings["folder_path"])
+
+    def load_settings(self):
+        default_settings = {"theme": "light", "folder_path": "/home/"}
+        if not self.settings_file.exists():
+            self.save_settings(default_settings)
+            return default_settings
+
+        with open(self.settings_file, "r", encoding="utf-8") as f:
+            return json.load(f)
+
+    def save_settings(self, data):
+        self.config_dir.mkdir(parents=True, exist_ok=True)
+
+        with open(self.settings_file, "w", encoding="utf-8") as f:
+            json.dump(data, f, indent=4)
+
+        self.default_settings = self.load_settings()
 
 
 if __name__ == "__main__":
